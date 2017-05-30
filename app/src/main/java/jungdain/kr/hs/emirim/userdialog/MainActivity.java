596e5fc2bdf816f963fad00444f4a2855dba6c8c
sunwonsara <sunwonsara@naver.com>
2017-05-30 19:27:23 +0900

    #3 Event Programming 2

--- app/src/main/java/jungdain/kr/hs/emirim/userdialog/MainActivity.java
+++ app/src/main/java/jungdain/kr/hs/emirim/userdialog/MainActivity.java
@@ -1,30 +1,56 @@
 package jungdain.kr.hs.emirim.userdialog;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+//toast: 일정시간동안 띄워주는 것
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    TextView textName, textEmail;
+    EditText editName, editEmail;
+    View dlgV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button but =(Button)findViewById(R.id.but);
         but.setOnClickListener(this);
+        textName=(TextView)findViewById(R.id.text_name);
+        textEmail=(TextView)findViewById(R.id.text_email);
     }
 
     @Override
     public void onClick(View v) {
-        View dlgV=View.inflate(getApplicationContext(),R.layout.dialog,null);
+        dlgV=View.inflate(getApplicationContext(),R.layout.dialog,null);
         AlertDialog.Builder dialog=new AlertDialog.Builder(this);
         dialog.setTitle("Input User Information");
         dialog.setIcon(R.mipmap.hart);
         dialog.setView(dlgV);
-        dialog.setPositiveButton("OK",null);
-        dialog.setNegativeButton("CANCEL",null);
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                editName=(EditText)dlgV.findViewById(R.id.edit_name);
+                editEmail=(EditText)dlgV.findViewById(R.id.edit_email);
+                textName.setText(editName.getText());
+                textEmail.setText(editEmail.getText());
+            }
+        });
+        dialog.setNegativeButton("CANCEL",new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                Toast toast = new Toast(MainActivity.this);
+                View toastV = View.inflate(MainActivity.this, R.layout.toast, null);
+                TextView textToast = (TextView) toastV.findViewById(R.id.text_toast);
+                textToast.setText("취소되었군요");
+                toast.setView(toastV); //우리가 만든 레이아웃 설정
+                toast.show();
+            }
+        });
         dialog.show();
     }
 }
